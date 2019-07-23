ae1bb5ba294c89b30d9c45660138b24dbe6d8d99
adhithyaakella <adhithya003@gmail.com>
2019-07-23 20:01:44 +0530

    MyFirstCode
    Signed-off-by: adhithyaakella <adhithya003@gmail.com>

---
+++ cucumber-project-skeleton/src/main/java/skeleton/MyCucuClass.java
@@ -0,0 +1,50 @@
+package skeleton;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class MyCucuClass {
+	@Given("Application URL is working")
+	public void application_URL_is_working() {
+	    // Write code here that turns the phrase above into concrete actions
+		System.out.println("Application URL is working");
+//	    throw new cucumber.api.PendingException();
+	}
+
+	@When("User enters userName")
+	public void user_enters_userName() {
+	    // Write code here that turns the phrase above into concrete actions
+		System.out.println("Application URL is working");
+//	    throw new cucumber.api.PendingException();
+	}
+
+	@When("USer enters pasword")
+	public void user_enters_pasword() {
+//	     Write code here that turns the phrase above into concrete actions
+//	    throw new cucumber.api.PendingException();
+		System.out.println("Application URL is working");
+	}
+
+	@Then("Clicks on submit button")
+	public void clicks_on_submit_button() {
+	    // Write code here that turns the phrase above into concrete actions
+//	    throw new cucumber.api.PendingException();
+		System.out.println("Application URL is working");
+	}
+
+	@Then("Verifies login Credentials")
+	public void verifies_login_Credentials() {
+	    // Write code here that turns the phrase above into concrete actions
+//	    throw new cucumber.api.PendingException();
+		System.out.println("Application URL is working");
+	}
+
+	@Then("He chose to close the application")
+	public void he_chose_to_close_the_application() {
+	    // Write code here that turns the phrase above into concrete actions
+//	    throw new cucumber.api.PendingException();
+		System.out.println("Application URL is working");
+	}
+
+}
